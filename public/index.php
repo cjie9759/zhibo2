4d3e04ec0fb9f3f9869cdf801f505fa72c39df67
cjie-qb <cjie9759@qq.com>
2021-10-13 11:49:29 +0800

    add index api base index

--- public/index.php
+++ public/index.php
@@ -1,2 +1,30 @@
 <?php
-phpinfo();
+// 引入对应class文件 建议采用自动加载的方式
+// include("../base.php");
+// include("../api.php");
+
+// 自动加载
+spl_autoload_register(function ($class_name) {
+    require_once '../' . $class_name . '.php';
+});
+
+
+// phpinfo();
+// pathinfo处理
+$pathinfo = explode('/', $_SERVER['PATH_INFO']);
+$s1 = $pathinfo[1] ?? 'index';
+$s2 = $pathinfo[2] ?? 'index';
+// $post = json_decode($_POST['json']) ?? $_POST;
+
+// 过滤非法请求 安全
+// define('SafeS', ['api', 'index']);
+// in_array($s1, SafeS) || exit('erro request');
+$safeS = [
+    "base" => ["test", "demo"],
+    "api" => ["getMsg", "upData"]
+];
+// isset($safeS[$s1]) && in_array($s2, $safeS[$s1]) ||  exit('erro request');
+
+// 实例化class、分发请求
+$demo = new $s1;
+echo $demo->$s2();
